e79e1146b4465fa1d8cd0515b330dd1b7791cd2d
cksgus <sejungpk@naver.com>
2018-08-07 02:09:58 +0900

    first upload

---
+++ README.md
@@ -0,0 +1,11 @@
+# Shooting-Game
+
+## Environment
+
+> **unity**:
+>
+> - version : 2018.2.2f1
+>
+> **c#**:
+>
+> - version : vs2017
---
+++ ShootingGame/Assets/Script/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (PlayerController))]
+public class Player : MonoBehaviour {
+
+    public float moveSpeed = 5;
+
+    Camera viewCamera;
+    PlayerController controller;
+
+	// Use this for initialization
+	void Start () {
+
+        controller = GetComponent<PlayerController> ();
+        viewCamera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")); //기본 스무딩 적용하지 않음
+        Vector3 moveVelocity = moveInput.normalized * moveSpeed;
+        controller.Move(moveVelocity);
+
+        Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayDistance;
+
+        if (groundPlane.Raycast(ray, out rayDistance))
+        {
+            Vector3 point = ray.GetPoint(rayDistance);
+            Debug.DrawLine(ray.origin,point,Color.red);
+            //controller.LookAt(point);
+        }
+    }
+}
